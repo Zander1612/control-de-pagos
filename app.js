9096a52db22549a06ffcbaa0bba94530a9501852
Lil-Gabo <gamedina.uesv@gmail.com>
2025-11-09 22:19:19 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI_TEST);
+        console.log('Conectado a MongoDB');
+    } catch (error) {
+        console.log('Error al conectar a MongoDB:', error);
+    }
+})();
+
+//Rutas Frontend
+app.use('/', express.static(path.resolve('views', 'home')))
+
+
+
+module.exports = app;
